e62c437263867b742e244c7471487992a469dd39
daymenu <hanjian2018@126.com>
2019-11-28 07:49:40 +0800

    增加日志文件

--- wechat/log.go
+++ wechat/log.go
@@ -3,8 +3,22 @@ package wechat
 import (
 	"log"
 	"os"
+	"path/filepath"
+	"time"
 )
 
 func getLogger() *log.Logger {
-	return log.New(os.Stdout, "wechat ", log.Ldate|log.Ltime)
+	logDir := "/var/log/wxapi/"
+	err := os.Mkdir(logDir, 0777)
+	isExit := os.IsExist(err)
+	if !isExit && err != nil {
+		log.Print(err)
+	}
+	time.Now()
+	filePath := filepath.Join(logDir, time.Now().Format("2006-01-02 15:04:05")+".log")
+	f, err := os.Create(filePath)
+	if !os.IsExist(err) && err != nil {
+		log.Print(err)
+	}
+	return log.New(f, "wechat ", log.Ldate|log.Ltime)
 }
