ffe03d9ab73c41f0daedc81b8dd70b1d83b05415
daymenu <hanjian2018@126.com>
2019-11-28 08:03:38 +0800

    log

--- wechat/log.go
+++ wechat/log.go
@@ -15,7 +15,7 @@ func getLogger() *log.Logger {
 		log.Print(err)
 	}
 	time.Now()
-	filePath := filepath.Join(logDir, time.Now().Format("2006-01-02 15:04:05")+".log")
+	filePath := filepath.Join(logDir, time.Now().Format("2006-01-02")+".log")
 	f, err := os.Create(filePath)
 	if !os.IsExist(err) && err != nil {
 		log.Print(err)
